34b185fe2a6a10e4bf6054621fcc18785cbdddb8
DE4DG4ZE <90854449+sutageiza0@users.noreply.github.com>
2023-12-24 10:13:03 +0100

    more words added to wordfilter

--- wordfilter.py
+++ wordfilter.py
@@ -1,1 +1,1 @@
-wordfilter = ['nigger', 'chink', 'nigga', 'nigguh', 'niggar', 'dyke', 'faggot', 'fag', 'kike', 'spic']
+wordfilter = ['nigger', 'chink', 'nigga', 'nigguh', 'niggar', 'dyke', 'faggot', 'fag', 'kike', 'spic', 'niggers', 'niggas', 'nig', 'nigs', 'kikes', 'fags', 'faggots', 'dykes', 'spics']
