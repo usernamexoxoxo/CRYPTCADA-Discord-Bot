f350d2d6cefc766190d7462197dcd3014870a151
DE4DG4ZE <90854449+sutageiza0@users.noreply.github.com>
2023-12-27 14:24:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -250,6 +250,8 @@ async def search_reddit(ctx, query):
                     displayed_posts.append(post.id)
                     # Remove the post from the new_posts list
                     new_posts.remove(post)
+                    # log
+                    print(f'Displayed Posts: {displayed_posts} New Posts: {new_posts}')
 
             if len(new_posts) == 0:
                 await send_embed_message(ctx, f'No more posts to display, please run the command again.', discord.Color.red())
